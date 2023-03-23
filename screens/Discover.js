d4d5818c27aba686f6e47e96b831cbca2ddf012b
Parag Jagdale <paragj2801@gmail.com>
2023-03-24 00:11:35 +0530

    new package  added react-native-google-places-autocomplete

--- screens/Discover.js
+++ screens/Discover.js
@@ -2,8 +2,8 @@ import { useNavigation } from "@react-navigation/native";
 import React, { useLayoutEffect } from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { HeroImage } from "../assest";
 import * as Animatable from "react-native-animatable";
+import { Avatar } from "../assest";
 
 export default Discover = () => {
   const navigation = useNavigation();
@@ -14,7 +14,15 @@ export default Discover = () => {
   }, []);
   return (
     <SafeAreaView className="bg-white flex-1 relative">
-      <Text>hii</Text>
+      <View className="flex-row items-center justify-between px-8">
+        <View>
+          <Text className="text-[40px] text-[#0B646B] font-bold">Discover</Text>
+          <Text className="text-[#527283] text-[36px]">The beauty tody</Text>
+        </View>
+        <View className="w-12 h-12 bg-gray-400 rounded-md items-center">
+          <Image source={Avatar} className="w-full h-full   shadow-lg" />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
